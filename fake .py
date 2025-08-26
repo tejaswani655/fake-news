bc4467881cbdfb62d8f546aa76e77e8d4e5eb29e
tejaswani655 <tejaswaniraj5@gmail.com>
2025-08-27 02:51:15 +0530

    Update fake .py

--- fake .py
+++ fake .py
@@ -11,7 +11,7 @@ news_input = st.text_area ("News Article:","")
 
 if st.button("Check News"):
     if news_input.strip():
-        transform_input = vectorizer.transform ([news_input])
+        transform_input = vectorization.transform ([news_input])
         prediction = model. predict (transform_input)
         if prediction [0]==1:
             st. success("The News is Real! ")
