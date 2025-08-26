20f9fc93f4b278c5ca9e50c33228fe916524f1d3
tejaswani655 <tejaswaniraj5@gmail.com>
2025-08-27 02:35:53 +0530

    Update fake .py

--- fake .py
+++ fake .py
@@ -13,9 +13,9 @@ if st.button("Check News"):
     if news_input.strip():
         transform_input = vectorizer.transform ([news_input])
         prediction = model. predict (transform_input)
-    if prediction [0]==1:
-        st. success("The News is Real! ")
-    else:
-        st.error("The News is Fake! ")
+        if prediction [0]==1:
+            st. success("The News is Real! ")
+        else:
+            st.error("The News is Fake! ")
     else:
         st.warning("Please enter some text to analyze. ")
